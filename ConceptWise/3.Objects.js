5f4176364d2d177f8ee09883954438d10764a25e
Jayaprakash Aluri <alurijayaprakash@gmail.com>
2023-10-19 14:04:28 +0530

    added new code

--- ConceptWise/3.Objects.js
+++ ConceptWise/3.Objects.js
@@ -18,3 +18,23 @@ console.log("keys" ,Object.keys(Person));
 console.log("Stringify" , JSON.stringify(Person));
 
 
+var User = {
+    firstName : "Jaya",
+    lastName : "Prakash",
+    role : "admin",
+    loginCount : 32,
+    facebookSignIn : true,
+    CourseList : [],
+    buyCourse : function (courseName) {
+        this.CourseList.push(courseName);
+    },
+    getCourseCount : function(){
+        return `${this.firstName} is enrolled in total of ${this.CourseList.length} courses`
+    },
+}
+
+console.log(User.firstName)
+User.buyCourse("Javascript Fundamentals");
+User.buyCourse("React Js");
+User.buyCourse("Angular");
+console.log(User.getCourseCount())
